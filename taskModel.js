ed72f416cda0e30127cb378ad2a3581f9fc61291
Ganeshkadali7337 <ganeshkadali0@gmail.com>
2024-05-01 19:47:35 +0530

    Update taskModel.js

--- taskModel.js
+++ taskModel.js
@@ -13,6 +13,10 @@ const Task = mongoose.Schema({
     type: String,
     required: true,
   },
+  assigneeName: {
+    type: String,
+    required: true,
+  },
   createdAt: {
     type: Date,
     default: Date.now,
